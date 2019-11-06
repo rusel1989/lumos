171a01eee59f592e795a7148a7664b0ce5d7d81c
Jan Šilhan <silhanu@gmail.com>
2019-11-06 22:05:05 +0100

    internal: pr fix

--- packages/config-danger/src/pr.ts
+++ packages/config-danger/src/pr.ts
@@ -14,7 +14,7 @@ export function checkForConventionalPrefix() {
 export function checkForConventionalSquashCommit() {
   if (
     danger.github.pr.commits <= 1 &&
-    danger.github.commits[0].commit.message.includes(danger.github.pr.title)
+    new RegExp(danger.github.commits[0].commit.message).test(danger.github.pr.title)
   ) {
     fail(
       'Automatic releases requires commit message to match PR title if PR contains only 1 commit.',
