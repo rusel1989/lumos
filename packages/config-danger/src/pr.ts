1900480c98b02363679264be312dc39d1f31fead
Jan Šilhan <silhanu@gmail.com>
2019-11-06 15:46:01 +0100

    internal: updated danger.

--- packages/config-danger/src/pr.ts
+++ packages/config-danger/src/pr.ts
@@ -1,10 +1,10 @@
-import { checkCommitFormat } from 'conventional-changelog-beemo';
+import { checkCommitFormat } from "conventional-changelog-beemo";
 
 // Verify the PR title contains the conventional-changelog required prefix.
 export function checkForConventionalPrefix() {
   if (!checkCommitFormat(danger.github.pr.title)) {
     fail(
-      'Pull request title requires a conventional changelog prefix. [View commit message format](https://github.com/beemojs/conventional-changelog-beemo#commit-message-format).',
+      "Pull request title requires a conventional changelog prefix. [View commit message format](https://github.com/beemojs/conventional-changelog-beemo#commit-message-format)."
     );
   }
 }
@@ -14,10 +14,10 @@ export function checkForConventionalPrefix() {
 export function checkForConventionalSquashCommit() {
   if (
     danger.github.pr.commits <= 1 &&
-    danger.github.commits[0].commit.message !== danger.github.pr.title
+    danger.github.commits[0].commit.message.includes(danger.github.pr.title)
   ) {
     fail(
-      'Automatic releases requires commit message to match PR title if PR contains only 1 commit.',
+      "Automatic releases requires commit message to match PR title if PR contains only 1 commit."
     );
   }
 }
