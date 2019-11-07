2dc4ecbda8cd7903d543ff756527435f84b7332c
Jan Šilhan <silhanu@gmail.com>
2019-11-07 16:32:58 +0100

    ci: another fix.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 exec 2>&1
 set -x
 
-npm install -g @beemo/cli
-yarn install --frozen-lockfile --ignore-engines
-yarn release
+npm install -g @beemo/cli 2>&1
+yarn install --frozen-lockfile --ignore-engines 2>&1
+yarn release 2>&1
