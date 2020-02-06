b6cbc35acc553b024901abd5c030758b1e195675
Jan Šilhan <silhanu@gmail.com>
2020-02-06 14:19:59 +0100

    fix: Webpack config.

--- packages/lumos/src/configs/webpack.ts
+++ packages/lumos/src/configs/webpack.ts
@@ -3,13 +3,11 @@ import { getSettings } from '@rajzik/lumos-common';
 
 const { srcFolder, react, entryPoint } = getSettings();
 
-const { context } = process.beemo;
-
 export = getConfig({
   analyzeBundle: !!process.env.WEBPACK_ANALYZE,
   port: process.env.PORT,
   react,
   sourceMaps: !!process.env.SOURCE_MAPS,
   srcFolder,
-  entryPoint: (context.args.entryPoint as string) || entryPoint,
+  entryPoint,
 });
