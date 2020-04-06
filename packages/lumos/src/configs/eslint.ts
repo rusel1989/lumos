f18bca1cd815f1d1708b6fc6963e106cdf9afc4b
Jan Šilhan <silhanu@gmail.com>
2020-04-06 13:48:49 +0200

    new: Add testing library to eslint.

--- packages/lumos/src/configs/eslint.ts
+++ packages/lumos/src/configs/eslint.ts
@@ -2,7 +2,7 @@ import { getExtendsList, getIgnoreList } from '@rajzik/config-eslint';
 import { getSettings } from '@rajzik/lumos-common';
 
 const { tool } = process.beemo;
-const { next, node } = getSettings();
+const { next, node, testingLibrary } = getSettings();
 
 export = {
   extends: getExtendsList({
@@ -10,6 +10,7 @@ export = {
     node,
     prettier: tool.isPluginEnabled('driver', 'prettier'),
     typescript: tool.isPluginEnabled('driver', 'typescript'),
+    testingLibrary,
   }),
   ignore: getIgnoreList(),
 };
