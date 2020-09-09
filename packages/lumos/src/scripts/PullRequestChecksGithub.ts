1a593cdb72d3da95ee090aacceda339b78c5fed6
Jan Šilhan <silhanu@gmail.com>
2020-09-09 13:06:55 +0200

    ci: New pipelines. (#642)
    * new: Pull request check.
    
    * new: Integration.
    
    * ci: Fix name.
    
    * ci: Fix formatting.
    
    * ci: Update workflows.
    
    * ci: Remove travis.
    
    * ci: Fix pull request.
    
    * ci: Fix pull request check.
    
    * ci: Update pull request check.
    
    * ci: Update pull request.
    
    * ci: Change to deps.
    
    * ci: Test context.
    
    * ci: Final test.
    
    * ci: Please work.
    
    * ci: Fix.
    
    * ci: Add github token.
    
    * ci: Test without token.
    
    * ci: Fix error.
    
    * ci: Update release.
    
    * ci: Fix token.
    
    * ci: Remove commented code.
    
    Co-authored-by: Silhan Jan <jsilhan@oriflame.com>

---
+++ packages/lumos/src/scripts/PullRequestChecksGithub.ts
@@ -0,0 +1,83 @@
+import { context } from '@actions/github';
+import { Script } from '@beemo/core';
+import { Octokit } from '@octokit/rest';
+import { checkCommitFormat } from '@rajzik/conventional-changelog-beemo';
+import path from 'path';
+import { createGitHubClient } from '../helpers/createGitHubClient';
+
+const { GITHUB_REF } = process.env;
+
+const parsePullRequestId = (githubRef: string) => {
+  const result = /refs\/pull\/(\d+)\/merge/g.exec(githubRef);
+  if (!result) throw new Error('Reference not found.');
+  const [, pullRequestId] = result;
+  return pullRequestId;
+};
+
+// Primarily used within CI jobs
+export default class PullRequestChecksScript extends Script {
+  owner!: string;
+
+  repo!: string;
+
+  client!: Octokit;
+
+  pullRequest!: string;
+
+  blueprint() {
+    return {};
+  }
+
+  bootstrap() {
+    this.pullRequest = parsePullRequestId(GITHUB_REF!);
+
+    if (this.pullRequest === 'false') {
+      return;
+    }
+    const {
+      repo: { owner, repo },
+    } = context;
+
+    this.owner = owner;
+    this.repo = repo;
+    this.client = createGitHubClient();
+
+    this.task('Checking for invalid lock file changes', this.checkForInvalidLocks);
+    this.task('Checking pull request title', this.checkForConventionalTitle);
+  }
+
+  async checkForInvalidLocks() {
+    const { data: files } = await this.client.pulls.listFiles({
+      owner: this.owner,
+      repo: this.repo,
+      pull_number: Number(this.pullRequest),
+    });
+
+    const fileNames = new Set(files.map(file => path.basename(file.filename)));
+    const hasPackageChanges = fileNames.has('package.json');
+
+    // this.tool.log('Changed files: %s', Array.from(fileNames).join(', '));
+
+    if (fileNames.has('package-lock.json') && !hasPackageChanges) {
+      throw new Error('Your PR contains changes to package-lock.json, but not package.json.');
+    } else if (fileNames.has('npm-shrinkwrap.json') && !hasPackageChanges) {
+      throw new Error('Your PR contains changes to npm-shrinkwrap.json, but not package.json.');
+    } else if (fileNames.has('yarn.lock') && !hasPackageChanges) {
+      throw new Error('Your PR contains changes to yarn.lock, but not package.json.');
+    }
+  }
+
+  async checkForConventionalTitle() {
+    const { data: pr } = await this.client.pulls.get({
+      owner: this.owner,
+      repo: this.repo,
+      pull_number: Number(this.pullRequest),
+    });
+
+    if (!checkCommitFormat(pr.title)) {
+      throw new Error(
+        'Pull request title requires a conventional changelog prefix. More information: https://github.com/rajzik/conventional-changelog-beemo#commit-message-format',
+      );
+    }
+  }
+}
