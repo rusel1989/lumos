f9fb144d27fd67f9c90878c7723ed984df3c3537
Silhan Jan <jsilhan@oriflame.com>
2020-12-03 13:04:51 +0100

    new: Add no-unsafe-assignment rule.

--- packages/config-eslint/src/presets/typescript.ts
+++ packages/config-eslint/src/presets/typescript.ts
@@ -144,6 +144,7 @@ const config: ESLintConfig = {
         '@typescript-eslint/no-unsafe-call': 'warn',
         '@typescript-eslint/no-unsafe-member-access': 'warn',
         '@typescript-eslint/no-unsafe-return': 'warn',
+        '@typescript-eslint/no-unsafe-assignment': 'warn',
         '@typescript-eslint/no-unused-vars': ['error', noUnused],
         '@typescript-eslint/no-use-before-define': 'error',
         '@typescript-eslint/no-shadow': 'error',
