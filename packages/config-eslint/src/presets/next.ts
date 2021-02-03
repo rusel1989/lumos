1cb71e253ebc6bc30540befe97e2086dbe8745bf
Jan Šilhan <silhanu@gmail.com>
2021-02-03 14:12:29 +0100

    internal: Sort properties and add coments. (#262)
    Co-authored-by: Jan Silhan <jsilhan@oriflame.com>

--- packages/config-eslint/src/presets/next.ts
+++ packages/config-eslint/src/presets/next.ts
@@ -4,7 +4,7 @@ import { EXTS_GROUP } from '@oriflame/lumos-common';
 const config: ESLintConfig = {
   plugins: ['promise', 'unicorn'],
   rules: {
-    // Not enabled in Airbnb
+    // not enabled in Airbnb
     'default-param-last': 'error',
     'func-name-matching': [
       'error',
@@ -25,40 +25,46 @@ const config: ESLintConfig = {
     'no-import-assign': 'error',
     'no-native-reassign': 'error',
     'no-negated-condition': 'error',
+    'no-nested-ternary': 'off', // replaced with unicorn rule
     'no-setter-return': 'error',
     'no-useless-call': 'error',
     'prefer-exponentiation-operator': 'error',
     'prefer-regex-literals': 'error',
     'require-atomic-updates': 'error',
-    'no-nested-ternary': 'off', // Replaced with unicorn rule
 
-    // Replaced with new proposals
+    // replaced with new proposals
     'react/jsx-props-no-spreading': 'off',
     'react/state-in-constructor': 'off',
     'react/static-property-placement': 'off',
 
-    // Import React is no longer needed from latest version
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-
-    // IMPORT
+    // import
     'import/default': 'error',
-    'import/namespace': 'error',
-    'import/no-unused-modules': 'off', // Super broken at the moment
     'import/imports-first': 'error',
+    'import/namespace': 'error',
+    'import/no-unused-modules': 'off', // super broken at the moment
 
-    // PROMISE
+    // promises
     'promise/no-callback-in-promise': 'error',
     'promise/no-new-statics': 'error',
     'promise/no-promise-in-callback': 'error',
     'promise/no-return-in-finally': 'error',
-    'promise/no-return-wrap': ['error', { allowReject: true }],
+    'promise/no-return-wrap': [
+      'error',
+      {
+        allowReject: true,
+      },
+    ],
     'promise/param-names': 'error',
     'promise/valid-params': 'error',
 
-    // REACT
-    'react/destructuring-assignment': 'off', // Broken with class properties
-    'react/forbid-prop-types': ['error', { forbid: ['any', 'array'] }],
+    // react
+    'react/destructuring-assignment': 'off',
+    'react/forbid-prop-types': [
+      'error',
+      {
+        forbid: ['any', 'array'],
+      },
+    ],
     'react/jsx-handler-names': [
       'error',
       {
@@ -67,9 +73,25 @@ const config: ESLintConfig = {
       },
     ],
     'react/jsx-key': 'error',
-    'react/jsx-no-literals': ['error', { noStrings: true, ignoreProps: true }],
-    'react/jsx-no-useless-fragment': 'error',
+    'react/jsx-no-bind': [
+      'warn',
+      {
+        allowArrowFunctions: false,
+        allowBind: false,
+        allowFunctions: false,
+        ignoreDOMComponents: false,
+        ignoreRefs: false,
+      },
+    ],
+    'react/jsx-no-literals': [
+      'error',
+      {
+        ignoreProps: true,
+        noStrings: true,
+      },
+    ],
     'react/jsx-no-script-url': 'error',
+    'react/jsx-no-useless-fragment': 'error',
     'react/jsx-sort-default-props': [
       'error',
       {
@@ -80,9 +102,9 @@ const config: ESLintConfig = {
       'error',
       {
         callbacksLast: true,
-        shorthandFirst: true,
         noSortAlphabetically: true,
         reservedFirst: true,
+        shorthandFirst: true,
       },
     ],
     'react/no-did-mount-set-state': 'error',
@@ -90,22 +112,8 @@ const config: ESLintConfig = {
     'react/sort-comp': [
       'error',
       {
-        order: ['statics', 'properties', 'lifecycle', 'everything-else', 'handlers', 'renderers'],
         groups: {
-          statics: [
-            'propTypes',
-            'defaultProps',
-            'contextType',
-            'contextTypes',
-            'childContextTypes',
-          ],
-          properties: [
-            '/^(?!on).+$/',
-            '/^(?!handle).+$/',
-            '/^(?!render).+$/',
-            '/^.+Ref$/',
-            'state',
-          ],
+          handlers: ['/^on.+$/', '/^handle.+$/'],
           lifecycle: [
             'constructor',
             'getDerivedStateFromProps',
@@ -122,33 +130,48 @@ const config: ESLintConfig = {
             'componentDidCatch',
             'componentWillUnmount',
           ],
-          handlers: ['/^on.+$/', '/^handle.+$/'],
+          properties: [
+            '/^(?!on).+$/',
+            '/^(?!handle).+$/',
+            '/^(?!render).+$/',
+            '/^.+Ref$/',
+            'state',
+          ],
           renderers: ['/^render.+$/', 'render'],
+          statics: [
+            'propTypes',
+            'defaultProps',
+            'contextType',
+            'contextTypes',
+            'childContextTypes',
+          ],
         },
+        order: ['statics', 'properties', 'lifecycle', 'everything-else', 'handlers', 'renderers'],
       },
     ],
     'react/sort-prop-types': [
       'error',
       {
-        ignoreCase: true,
         callbacksLast: true,
+        ignoreCase: true,
         requiredFirst: false,
         sortShapeProp: true,
       },
     ],
-    'react/jsx-no-bind': [
+    // unicorn
+    'unicorn/better-regex': 'error', // improve regexps by making them shorter, consistent, and safer
+    'unicorn/catch-error-name': 'error', // enforce a specific parameter name in catch clauses
+    'unicorn/consistent-function-scoping': 'error', // move function definitions to the highest possible scope
+    'unicorn/custom-error-definition': 'error', // enforce correct Error subclassing
+    'unicorn/error-message': 'error', // enforce passing a message value when throwing a built-in error
+    'unicorn/escape-case': 'error', // require escape sequences to use uppercase values
+    'unicorn/expiring-todo-comments': [
       'warn',
       {
-        ignoreDOMComponents: false,
-        ignoreRefs: false,
-        allowArrowFunctions: false,
-        allowFunctions: false,
-        allowBind: false,
+        terms: ['todo:', 'fixme:', 'debug:'],
       },
-    ],
-
-    // UNICORN
-    'unicorn/better-regex': 'error',
+    ], // add expiration conditions to to do comments
+    'unicorn/explicit-length-check': 'error', // enforce explicitly comparing the length property of a value
     'unicorn/filename-case': [
       'warn',
       {
@@ -157,53 +180,41 @@ const config: ESLintConfig = {
           pascalCase: true,
         },
       },
-    ],
-    'unicorn/catch-error-name': 'error',
-    'unicorn/consistent-function-scoping': 'error',
-    'unicorn/custom-error-definition': 'error',
-    'unicorn/error-message': 'error',
-    'unicorn/escape-case': 'error',
-    'unicorn/explicit-length-check': 'error',
-    'unicorn/import-index': 'error',
-    'unicorn/new-for-builtins': 'error',
-    'unicorn/no-abusive-eslint-disable': 'warn',
-    'unicorn/no-instanceof-array': 'error',
-    'unicorn/no-hex-escape': 'error',
-    'unicorn/no-array-callback-reference': 'error',
-    'unicorn/no-for-loop': 'error',
-    'unicorn/no-new-buffer': 'error',
-    'unicorn/no-process-exit': 'error',
-    'unicorn/no-zero-fractions': 'error',
-    'unicorn/number-literal-case': 'error',
-    'unicorn/prefer-add-event-listener': 'error',
-    'unicorn/prefer-array-find': 'error',
-    'unicorn/prefer-dom-node-dataset': 'error',
-    'unicorn/prefer-keyboard-event-key': 'error',
-    'unicorn/prefer-array-flat-map': 'error',
-    'unicorn/prefer-includes': 'error',
-    'unicorn/prefer-modern-dom-apis': 'error',
-    'unicorn/prefer-negative-index': 'error',
-    'unicorn/prefer-dom-node-append': 'error',
-    'unicorn/prefer-dom-node-remove': 'error',
-    'unicorn/prefer-string-starts-ends-with': 'error',
-    'unicorn/prefer-string-slice': 'error',
-    'unicorn/prefer-dom-node-text-content': 'error',
-    'unicorn/prefer-string-trim-start-end': 'error',
-    'unicorn/prefer-type-error': 'error',
-    'unicorn/throw-new-error': 'error',
-    'unicorn/prefer-string-replace-all': 'error',
-    'unicorn/prefer-set-has': 'error',
-    'unicorn/prefer-reflect-apply': 'error',
-    'unicorn/prefer-number-properties': 'error',
-    'unicorn/no-unused-properties': 'warn',
-    'unicorn/no-object-as-default-parameter': 'warn',
-    'unicorn/no-nested-ternary': 'error',
-    'unicorn/expiring-todo-comments': [
-      'warn',
-      {
-        terms: ['todo:', 'fixme:'],
-      },
-    ],
+    ], // enforce a case style for filenames
+    'unicorn/import-index': 'error', // enforce importing index files with .
+    'unicorn/new-for-builtins': 'error', // enforce the use of new for all builtins, except String, Number, Boolean, Symbol and BigInt
+    'unicorn/no-abusive-eslint-disable': 'warn', // enforce specifying rules to disable in eslint-disable comments
+    'unicorn/no-array-callback-reference': 'error', // prevent passing a function reference directly to iterator methods
+    'unicorn/no-for-loop': 'error', // Do not use a for loop that can be replaced with a for-of loop
+    'unicorn/no-hex-escape': 'error', // enforce the use of Unicode escapes instead of hexadecimal escapes
+    'unicorn/no-instanceof-array': 'error', // require Array.isArray() instead of instanceof Array
+    'unicorn/no-nested-ternary': 'error', // disallow nested ternary expressions
+    'unicorn/no-new-buffer': 'error', // enforce the use of Buffer.from() and Buffer.alloc() instead of the deprecated new Buffer()
+    'unicorn/no-object-as-default-parameter': 'warn', // disallow the use of objects as default parameters
+    'unicorn/no-process-exit': 'error', // disallow process.exit()
+    'unicorn/no-unused-properties': 'warn', // disallow unused object properties
+    'unicorn/no-zero-fractions': 'error', // disallow number literals with zero fractions or dangling dots
+    'unicorn/number-literal-case': 'error', // enforce lowercase identifier and uppercase value for number literals
+    'unicorn/prefer-add-event-listener': 'error', // prefer .addEventListener() and .removeEventListener() over on-functions
+    'unicorn/prefer-array-find': 'error', // prefer .find(…) over the first element from .filter(…)
+    'unicorn/prefer-array-flat-map': 'error', // prefer .flatMap(…) over .map(…).flat()
+    'unicorn/prefer-dom-node-append': 'error', // prefer Node#append() over Node#appendChild()
+    'unicorn/prefer-dom-node-dataset': 'error', // prefer using .dataset on DOM elements over .setAttribute(…)
+    'unicorn/prefer-dom-node-remove': 'error', // prefer childNode.remove() over parentNode.removeChild(childNode)
+    'unicorn/prefer-dom-node-text-content': 'error', // prefer .textContent over .innerText
+    'unicorn/prefer-includes': 'error', // prefer .includes() over .indexOf() when checking for existence or non-existence
+    'unicorn/prefer-keyboard-event-key': 'error', // prefer KeyboardEvent#key over KeyboardEvent#keyCode.
+    'unicorn/prefer-modern-dom-apis': 'error', // prefer .before() over .insertBefore(), .replaceWith() over .replaceChild(), prefer one of .before(), .after(), .append() or .prepend() over insertAdjacentText() and insertAdjacentElement()
+    'unicorn/prefer-negative-index': 'error', // prefer negative index over .length - index for {String,Array,TypedArray}#slice() and Array#splice()
+    'unicorn/prefer-number-properties': 'error', // prefer Number static properties over global ones
+    'unicorn/prefer-reflect-apply': 'error', // prefer Reflect.apply() over Function#apply()
+    'unicorn/prefer-set-has': 'error', // prefer Set#has() over Array#includes() when checking for existence or non-existence
+    'unicorn/prefer-string-replace-all': 'error', // prefer String#replaceAll() over regex searches with the global flag
+    'unicorn/prefer-string-slice': 'error', // prefer String#slice() over String#substr() and String#substring()
+    'unicorn/prefer-string-starts-ends-with': 'error', // prefer String#startsWith() & String#endsWith() over more complex alternatives
+    'unicorn/prefer-string-trim-start-end': 'error', // prefer String#trimStart() / String#trimEnd() over String#trimLeft() / String#trimRight()
+    'unicorn/prefer-type-error': 'error', // enforce throwing TypeError in type checking conditions
+    'unicorn/throw-new-error': 'error', // require new when throwing an error
   },
 
   overrides: [
--- packages/config-eslint/src/presets/typescript.ts
+++ packages/config-eslint/src/presets/typescript.ts
@@ -32,16 +32,16 @@ const config: ESLintConfig = {
       plugins: ['@typescript-eslint'],
 
       rules: {
+        camelcase: 'off',
         'func-call-spacing': 'off',
         'no-restricted-globals': 'off',
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'no-unused-expressions': 'off',
         'no-unused-vars': 'off',
-        'no-unused-expressions': 'off', // Replaced with typescript version
-        camelcase: 'off', // Replaced with typescript version
-        'no-shadow': 'off', // Broken at the moment
         'no-use-before-define': 'off',
-        'no-undef': 'off', // Broken at the moment
 
-        // IMPORT (Conflicts with TS patterns)
+        // import (Conflicts with TS patterns)
         'import/extensions': [
           'error',
           'never',
@@ -49,10 +49,9 @@ const config: ESLintConfig = {
             json: 'always',
           },
         ],
+        'import/prefer-default-export': 'off', // Typescript will handle named export better than default
         'import/named': 'off',
         'import/no-cycle': 'off',
-        'import/no-named-as-default': 'off',
-        'import/prefer-default-export': 'off', // Typescript will handle named export better than default
         'import/no-extraneous-dependencies': [
           'error',
           {
@@ -67,8 +66,9 @@ const config: ESLintConfig = {
             optionalDependencies: false,
           },
         ],
+        'import/no-named-as-default': 'off',
 
-        // REACT (We don't use prop types)
+        // react (We don't use prop types)
         'react/default-props-match-prop-types': 'off',
         'react/jsx-filename-extension': [
           'error',
@@ -80,89 +80,72 @@ const config: ESLintConfig = {
         'react/prop-types': 'off',
         'react/require-default-props': 'off',
 
-        // UNICORN
+        // react
         'unicorn/no-fn-reference-in-iterator': 'off',
 
-        // TYPESCRIPT
-        '@typescript-eslint/adjacent-overload-signatures': 'error',
-        '@typescript-eslint/await-thenable': 'error',
-        // '@typescript-eslint/class-name-casing': ['error', { allowUnderscorePrefix: true }],
-        '@typescript-eslint/consistent-type-assertions': [
-          'error',
-          { assertionStyle: 'as', objectLiteralTypeAssertions: 'allow-as-parameter' },
-        ],
-        '@typescript-eslint/explicit-function-return-type': 'off', // Allow inferrence
-        '@typescript-eslint/func-call-spacing': ['error', 'never'],
-        '@typescript-eslint/member-delimiter-style': 'error',
-        '@typescript-eslint/member-ordering': 'off', // Prefer react/sort-comp
-        '@typescript-eslint/no-array-constructor': 'error',
-        '@typescript-eslint/array-type': [
-          'warn',
-          {
-            default: 'array-simple',
-          },
-        ],
-        '@typescript-eslint/consistent-type-imports': [
-          'warn',
-          {
-            prefer: 'no-type-imports',
-          },
-        ],
-        '@typescript-eslint/no-empty-function': 'off', // Default props are usually empty
-        '@typescript-eslint/no-empty-interface': 'error',
+        // typescript
+
+        '@typescript-eslint/adjacent-overload-signatures': 'error', // require that member overloads be consecutive
+        '@typescript-eslint/array-type': ['warn', { default: 'array-simple' }], // requires using either T[] or Array<T> for arrays
+        '@typescript-eslint/await-thenable': 'error', // disallows awaiting a value that is not a Thenable
         '@typescript-eslint/ban-types': [
           'error',
           {
             types: {
               Function:
                 "Don't use Function as a type. Use explicit types like `(...args: any[]) => void`",
-              String: {
-                message: 'Use string instead',
-                fixWith: 'string',
-              },
+              String: { message: 'Use string instead', fixWith: 'string' },
             },
           },
-        ],
-        '@typescript-eslint/no-inferrable-types': 'warn',
-        '@typescript-eslint/method-signature-style': ['warn', 'method'],
+        ], // bans specific types from being used
+        '@typescript-eslint/consistent-type-assertions': [
+          'error',
+          { assertionStyle: 'as', objectLiteralTypeAssertions: 'allow-as-parameter' },
+        ], // enforces consistent usage of type assertions
+        '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'no-type-imports' }], // enforces consistent usage of type imports
+        '@typescript-eslint/explicit-function-return-type': 'off', // require explicit return types on functions and class methods. Allow inference
+        '@typescript-eslint/func-call-spacing': ['error', 'never'], // require or disallow spacing between function identifiers and their invocations
+        '@typescript-eslint/member-delimiter-style': 'error', // require a specific member delimiter style for interfaces and type literals
+        '@typescript-eslint/member-ordering': 'off', // require a consistent member declaration order
+        '@typescript-eslint/method-signature-style': ['warn', 'method'], // enforces using a particular method signature syntax.
+        '@typescript-eslint/no-array-constructor': 'error', // disallow generic Array constructors
+        '@typescript-eslint/no-empty-function': 'off', // disallow empty functions. Default props are usually empty
+        '@typescript-eslint/no-empty-interface': 'error', // disallow the declaration of empty interfaces
         '@typescript-eslint/no-explicit-any': [
           'error',
           { fixToUnknown: false, ignoreRestArgs: true },
-        ],
-        '@typescript-eslint/prefer-includes': 'warn',
-        '@typescript-eslint/prefer-regexp-exec': 'warn',
-        '@typescript-eslint/prefer-ts-expect-error': 'warn',
-        '@typescript-eslint/no-unnecessary-type-assertion': 'error',
-        '@typescript-eslint/no-extra-parens': 'error',
-        '@typescript-eslint/no-for-in-array': 'error',
-        '@typescript-eslint/no-misused-new': 'error',
-        '@typescript-eslint/no-misused-promises': 'error',
-        '@typescript-eslint/no-parameter-properties': 'error',
-        '@typescript-eslint/no-require-imports': 'error',
-        '@typescript-eslint/no-throw-literal': 'error',
-        '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
-        '@typescript-eslint/no-unsafe-call': 'error',
-        '@typescript-eslint/no-unsafe-member-access': 'error',
-        '@typescript-eslint/no-unsafe-return': 'error',
-        '@typescript-eslint/no-unsafe-assignment': 'error',
-        '@typescript-eslint/no-unused-vars': ['error', noUnused],
-        '@typescript-eslint/no-use-before-define': 'error',
-        '@typescript-eslint/no-shadow': 'error',
-        '@typescript-eslint/prefer-as-const': 'error',
-        '@typescript-eslint/prefer-for-of': 'error',
-        '@typescript-eslint/prefer-namespace-keyword': 'error',
-        '@typescript-eslint/prefer-nullish-coalescing': 'off', // Lots of false positives
-        '@typescript-eslint/prefer-optional-chain': 'error',
-        '@typescript-eslint/promise-function-async': 'off', // Conflicts with other async rules
-        '@typescript-eslint/require-await': 'error',
-        '@typescript-eslint/switch-exhaustiveness-check': 'error',
-        '@typescript-eslint/triple-slash-reference': 'error',
-        '@typescript-eslint/type-annotation-spacing': 'error',
-        '@typescript-eslint/unified-signatures': 'error',
-        // '@typescript-eslint/prefer-nullish-coalescing': [
-        //   'warn',
-        //   { ignoreConditionalTests: false, ignoreMixedLogicalExpressions: false },
-        // ],
+        ], // disallow usage of the any type
+        '@typescript-eslint/no-extra-parens': 'error', // disallow unnecessary parentheses
+        '@typescript-eslint/no-for-in-array': 'error', // disallow iterating over an array with a for-in loop
+        '@typescript-eslint/no-inferrable-types': 'warn', // disallows explicit type declarations for variables or parameters initialized to a number, string, or boolean
+        '@typescript-eslint/no-misused-new': 'error', // enforce valid definition of new and constructor
+        '@typescript-eslint/no-misused-promises': 'error', // avoid using promises in places not designed to handle them
+        '@typescript-eslint/no-parameter-properties': 'error', // disallow the use of parameter properties in class constructors
+        '@typescript-eslint/no-require-imports': 'error', // disallows invocation of require()
+        '@typescript-eslint/no-shadow': 'error', // disallow variable declarations from shadowing variables declared in the outer scope
+        '@typescript-eslint/no-throw-literal': 'error', // disallow throwing literals as exceptions
+        '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error', // flags unnecessary equality comparisons against boolean literals
+        '@typescript-eslint/no-unnecessary-type-assertion': 'error', // warns if a type assertion does not change the type of an expression
+        '@typescript-eslint/no-unsafe-assignment': 'error', // disallows assigning any to variables and properties
+        '@typescript-eslint/no-unsafe-call': 'error', // disallows calling an any type value
+        '@typescript-eslint/no-unsafe-member-access': 'error', // disallows member access on any typed variables
+        '@typescript-eslint/no-unsafe-return': 'error', // disallows returning any from a function
+        '@typescript-eslint/no-unused-vars': ['error', noUnused], // disallow unused variables
+        '@typescript-eslint/no-use-before-define': 'error', // disallow the use of variables before they are defined
+        '@typescript-eslint/prefer-as-const': 'error', // prefer usage of as const over literal type
+        '@typescript-eslint/prefer-for-of': 'error', // prefer a ‘for-of’ loop over a standard ‘for’ loop if the index is only used to access the array being iterated
+        '@typescript-eslint/prefer-includes': 'warn', // enforce includes method over indexOf method
+        '@typescript-eslint/prefer-namespace-keyword': 'error', // require the use of the namespace keyword instead of the module keyword to declare custom TypeScript modules
+        '@typescript-eslint/prefer-nullish-coalescing': 'off', // enforce the usage of the nullish coalescing operator instead of logical chaining. Lots of false positives
+        '@typescript-eslint/prefer-optional-chain': 'error', // Prefer using concise optional chain expressions instead of chained logical ands
+        '@typescript-eslint/prefer-regexp-exec': 'warn', // prefer RegExp#exec() over String#match() if no global flag is provided
+        '@typescript-eslint/prefer-ts-expect-error': 'warn', // recommends using // @ts-expect-error over // @ts-ignore
+        '@typescript-eslint/promise-function-async': 'off', // requires any function or method that returns a Promise to be marked async. Conflicts with other async rules
+        '@typescript-eslint/require-await': 'error', // disallow async functions which have no await expression
+        '@typescript-eslint/switch-exhaustiveness-check': 'error', // exhaustiveness checking in switch with union type
+        '@typescript-eslint/triple-slash-reference': 'error', // sets preference level for triple slash directives versus ES6-style import declarations
+        '@typescript-eslint/type-annotation-spacing': 'error', // require consistent spacing around type annotations
+        '@typescript-eslint/unified-signatures': 'error', // warns for any two overloads that could be unified into one by using a union or an optional/rest parameter
       },
     },
   ],
