a6891d9e603bf44e8ed87cd3e5f8f6acd8c6373e
Jan Silhan <jsilhan@oriflame.com>
2021-04-27 13:39:22 +0200

    fix: Remove next from package json.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   "extends": [
     "./packages/config-eslint/lib/presets/base",
-    "./packages/config-eslint/lib/presets/next",
     "./packages/config-eslint/lib/presets/node",
     "./packages/config-eslint/lib/presets/typescript",
     "./packages/config-eslint/lib/presets/prettier",
