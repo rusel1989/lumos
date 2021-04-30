c0d7289a42d6bddb829c7bb4c0c262aa8702a8ea
Jan Silhan <jsilhan@oriflame.com>
2021-04-30 12:04:54 +0200

    internal: Change chunkfilename back to id.

--- packages/config-webpack/src/index.ts
+++ packages/config-webpack/src/index.ts
@@ -46,7 +46,7 @@ export function getConfig({
     path: internalPath,
     publicPath,
     filename: '[name].js',
-    chunkFilename: PROD ? '[base].[contenthash:8].chunk.js' : '[base].js',
+    chunkFilename: PROD ? '[id].[contenthash:8].chunk.js' : '[id].js',
     sourceMapFilename: '[file].map',
     uniqueName: PROD ? getUniqueName() : undefined,
   };
